3da4bf693328e0ae3dbed2785d0612b0c65dbde7
camCampus <camille.bochar@le-campus-numerique.fr>
2022-11-10 14:27:55 +0100

    api fetch

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 // Exemple d'appel JS pour récupérer des données météo :
 //
-// fetch("mon_api_json_meteo").then((resp) => {
-//   return resp.json();
-// }).then((data) => {
-//   console.log('data: ' + JSON.stringify(data));
-// });
+fetch("https://api.meteo-concept.com/api/forecast/daily?token=a352bbb36f25dcddce2c190e489e6a9b2b3f7677966a9380a873a8a879851903&insee=73005").then((resp) => {
+  return resp.json();
+}).then((data) => {
+  console.log('data: ' + JSON.stringify(data));
+  var city = data.city.name;
+
+
+  console.log("city= "+ city);
+
+});
+
